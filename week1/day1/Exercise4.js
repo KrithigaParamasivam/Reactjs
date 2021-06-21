817929f8dd401474c2103ecf5e32971cd2a30d94
KrithigaP <86260992+KrithigaParamasivam@users.noreply.github.com>
2021-06-22 00:20:23 +0530

    Add files via upload

---
+++ week1/day1/Exercise4.js
@@ -0,0 +1,9 @@
+let a='1';
+let num=1;
+let b=true;
+let result1=a==num;
+let result2=num==a;
+let result3=num===a;
+console.log("Using ==: \n"+"Is value s1 equal to num1: "+result1);
+console.log("Using ==: \n"+"Is value num1 equal to s2: "+result2);
+console.log("Using ===: \n"+"Is value num1 equal to s2: "+result3);
